6d2593278e96c84d1615d23f7c4bda1db16fd806
aldin <changhyunlee96@gmail.com>
2024-05-16 17:49:33 +0900

    [back_240516] 구글로그인 연동

--- backend/src/main/java/com/ssafit/controller/AuthRestController.java
+++ backend/src/main/java/com/ssafit/controller/AuthRestController.java
@@ -9,77 +9,87 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.ssafit.model.dto.User;
 import com.ssafit.model.service.UserService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/oauth2")
-@Tag(name = "authRestController", description = "구글 로그인 관련 컨트롤러")
 public class AuthRestController {
 
-	@Value("${google.client.id}")
-	private String clientId;
+    @Value("${google.client.id}")
+    private String clientId;
 
-	@Value("${google.client.secret}")
-	private String clientSecret;
+    @Value("${google.client.secret}")
+    private String clientSecret;
 
-	@Value("${google.redirect.uri}")
-	private String redirectUri;
+    @Value("${google.redirect.uri}")
+    private String redirectUri;
 
-	@Autowired
-	private UserService userService;
+    @Autowired
+    private UserService userService;
 
-	@PostMapping("/callback")
-	@Operation(summary = "사용자 정보를 받아서 프론트엔드에 다시 전달.")
-	public ResponseEntity<Map<String, String>> googleCallback(@RequestParam String code, HttpSession session) {
-		RestTemplate restTemplate = new RestTemplate();
+    @PostMapping("/callback")
+    public ResponseEntity<Map<String, String>> googleCallback(@RequestBody Map<String, String> requestBody, HttpSession session) {
+        String code = requestBody.get("code");
+        String redirectUri = requestBody.get("redirect_uri");
+        RestTemplate restTemplate = new RestTemplate();
 
-		String tokenUrl = "https://oauth2.googleapis.com/token";
-		Map<String, String> tokenRequest = new HashMap<>();
-		tokenRequest.put("code", code);
-		tokenRequest.put("client_id", clientId);
-		tokenRequest.put("client_secret", clientSecret);
-		tokenRequest.put("redirect_uri", redirectUri);
-		tokenRequest.put("grant_type", "authorization_code");
+        // Exchange the authorization code for an access token
+        String tokenUrl = "https://oauth2.googleapis.com/token";
+        Map<String, String> tokenRequest = new HashMap<>();
+        tokenRequest.put("code", code);
+        tokenRequest.put("client_id", clientId);
+        tokenRequest.put("client_secret", clientSecret);
+        tokenRequest.put("redirect_uri", redirectUri);
+        tokenRequest.put("grant_type", "authorization_code");
 
-		Map<String, String> tokenResponse = restTemplate.postForObject(tokenUrl, tokenRequest, Map.class);
-		String accessToken = tokenResponse.get("access_token");
+        Map<String, String> tokenResponse = restTemplate.postForObject(tokenUrl, tokenRequest, Map.class);
+        String accessToken = tokenResponse.get("access_token");
 
-		String userInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + accessToken;
-		Map<String, String> userInfoResponse = restTemplate.getForObject(userInfoUrl, Map.class);
+        // Use the access token to get user info
+        String userInfoUrl = "https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + accessToken;
+        Map<String, String> userInfoResponse = restTemplate.getForObject(userInfoUrl, Map.class);
 
-		String openId = userInfoResponse.get("id");
-		String nickname = userInfoResponse.get("name");
-		String profileImage = userInfoResponse.get("picture");
-		Date birth = Date.valueOf(userInfoResponse.get("birthday"));
+        // Get user info from response
+        String openId = userInfoResponse.get("id");
+        String nickname = userInfoResponse.get("name");
+        String profileImage = userInfoResponse.get("picture");
+        String birth = userInfoResponse.get("birthday");
+        
+        // 생년월일 파싱
+        Date birthDate = null;
+        if (birth != null) {
+            birthDate = Date.valueOf(birth);
+        }
 
-		User user = userService.getUserByTag(openId);
-		if (user == null) {
-			user = new User();
-			user.setUserTag(openId);
-			user.setUserNickname(nickname);
-			user.setUserBirth(birth);
-			user.setUserProfileImage(profileImage);
-			userService.createUser(user);
-		}
+        // Create or update the user in the database
+        User user = userService.getUserByTag(openId);
+        if (user == null) {
+            user = new User();
+            user.setUserTag(openId);
+            user.setUserNickname(nickname);
+            user.setUserBirth(birthDate);
+            user.setUserProfileImage(profileImage);
+            userService.createUser(user);
+        }
 
-		session.setAttribute("user", user);
+        // Save user info in session
+        session.setAttribute("user", user);
 
-		Map<String, String> response = new HashMap<>();
-		response.put("userTag", user.getUserTag());
-		response.put("userNickname", user.getUserNickname());
-		response.put("userProfileImage", user.getUserProfileImage());
-		response.put("userBirth", user.getUserBirth().toString());
+        // Return user info to frontend
+        Map<String, String> response = new HashMap<>();
+        response.put("userTag", user.getUserTag());
+        response.put("userNickname", user.getUserNickname());
+        response.put("userProfileImage", user.getUserProfileImage());
+        response.put("userBirth", user.getUserBirth() != null ? user.getUserBirth().toString() : null);
 
-		return new ResponseEntity<>(response, HttpStatus.OK);
-	}
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
