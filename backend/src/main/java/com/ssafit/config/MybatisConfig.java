838529e86e5bc19e993f9ff38ac8e320ea695d96
dongyeop <leedy977@gmail.com>
2024-05-22 12:12:34 +0900

    123

--- backend/src/main/java/com/ssafit/config/MybatisConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ssafit.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.ssafit.model.dao")
-public class MybatisConfig {
-}
